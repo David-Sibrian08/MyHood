66a0d45a80c0da739dc9ac92a6f69e498d22fd2f
David Sibrian <davidsibrian08@gmail.com>
2016-09-08 18:01:31 -0400

    Implemented the main view controller

--- MyHood/ViewController.swift
+++ MyHood/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let post1 = Post(imagePath: "", title: "POST 1", description: "Test post for testing purposes")
+        posts.append(post1)
+        
+        let post2 = Post(imagePath: "", title: "Post 2", description: "THE THE THE THE THE THE THE THE THE THE THE THE THE")
+        posts.append(post2)
+        
+        let post3 = Post(imagePath: "", title: "Post 3", description: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        
+        posts.append(post3)
+        
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        
+        //if there is a cell available to use, give it and make it a PostCell
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+        } else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 
 }
 
